d12e60b2430a1c2da7d966814b3ec15abbbb09e2
Simeon Kolchin <simeon.kolchin@icloud.com>
2024-06-23 22:09:55 +0300

    ml.py новый

--- app/ml/ml.py
+++ app/ml/ml.py
@@ -1,5 +1,5 @@
 from ml.classification_direction import DirectionClassificator
-from ml.ocr import OCRModel
+from ml.ocr import OCR
 from ml.search_people import SearchPeople
 from app.ml.logo_detector import LogoDetector
 from ml.classification_crop import CropClassificator
@@ -10,7 +10,7 @@ from PIL import Image, ImageDraw
 class LogoErrorChecker:
     def __init__(self):
         self.direction_classificator = DirectionClassificator()
-        # self.ocr_model = Reader(['ru'], gpu=True)
+        self.ocr_model = OCR()
         self.people_searcher = SearchPeople()
         # self.crop_classificatior = CropClassificator()
         self.logo_detector = LogoDetector()
@@ -35,6 +35,10 @@ class LogoErrorChecker:
             errors.append('Найден человек на фото')
 
         
+        ocr_class = self.ocr_model.predict(image_path)
+        # Вот с этим классом потом что нибудь будем делать
+
+        
         # classification_result = self.classifier.predict(image_path)
         # if classification_result < 0:
         #     errors.append("Ошибка отображения: логотип расположен неправильно или имеет деффект с размер")
@@ -46,3 +50,5 @@ if __name__ == "__main__":
     checker = LogoErrorChecker()
 
     errors = checker.check_errors('/home/dmitrii/Desktop/hack_22_06_24_school_of_programming/data/original/demographics/Примеры некорректного брендирования/9 н.jpg')
+
+    print(errors)
