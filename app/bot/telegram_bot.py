b1d6e5f709399ce4ecc91365abbf7d65ff6e6e7f
Simeon Kolchin <sskolchin@edu.hse.ru>
2024-06-23 20:56:36 +0300

    structurize code, add telegram bot, add streamlit

---
+++ app/bot/telegram_bot.py
@@ -0,0 +1,55 @@
+import os
+from aiogram import Bot, Dispatcher, types, Router
+from aiogram.types import Message, ContentType
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import CommandStart
+from aiogram import F
+
+from ml.ml import LogoErrorChecker
+from PIL import Image
+import io
+
+API_TOKEN = '7207561115:AAEMfuR0dRTIfQT3jxLk_yGxskAqMBEBaJQ'
+
+# Инициализация бота и диспетчера
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
+router = Router()
+
+# Инициализация класса проверки ошибок
+checker = LogoErrorChecker(
+    classification_model_path='..\ml\weights\mobilenet_v2.onnx',
+    yolo_people_model_path='path_to_yolo_people_model.pt'
+)
+
+@router.message(CommandStart())
+async def send_welcome(message: Message):
+    await message.answer("Welcome to the Logo Error Checker Bot! Send an image to get started.")
+
+@router.message(F.content_type == ContentType.PHOTO)
+async def handle_image(message: Message):
+    photo = message.photo[-1]
+    photo_file = await bot.get_file(photo.file_id)
+    photo_bytes = await bot.download_file(photo_file.file_path)
+    
+    image = Image.open(io.BytesIO(photo_bytes)).convert('RGB')
+    image.save("temp.jpg")
+
+    # Предсказание
+    error_list = checker.check_errors("temp.jpg")
+    os.remove("temp.jpg")
+    
+    if error_list:
+        response = "Detected errors:\n" + "\n".join(error_list)
+    else:
+        response = "No errors detected."
+    
+    await message.answer(response)
+
+def run_bot():
+    dp.include_router(router)
+    dp.start_polling(bot, skip_updates=True)
+
+if __name__ == "__main__":
+    run_bot()
--- app/ui/layout.py
+++ app/ui/layout.py
@@ -1,23 +1,29 @@
 import streamlit as st
 from PIL import Image
-from utils.utils import preprocess_image
-from ml.classification_direction import ImageClassifierONNX
+from ml.ml import LogoErrorChecker
 
-def render_layout(models):
-    st.title("Image Prediction App")
+def main():
+    st.title('Image Classification with ONNX Model')
+    st.write('Upload an image to classify it using the ONNX model.')
 
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
-    option1 = st.selectbox("Select Option 1", ["Option 1A", "Option 1B", "Option 1C"])
-    option2 = st.selectbox("Select Option 2", ["Option 2A", "Option 2B", "Option 2C"])
-
-    selected_model = st.selectbox("Select Model", list(models.keys()))
+    # Загрузка изображения
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
     if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        
-        if st.button("Predict"):
-            model = models[selected_model]
-            processed_image = preprocess_image(image)
-            result = model(processed_image)
-            st.write("Prediction result:", result)
+        image = Image.open(uploaded_file).convert('RGB')
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("Classifying...")
+
+        # Инициализация класса проверки ошибок
+        checker = LogoErrorChecker(
+            classification_model_path='weights/mobilenet_v2.onnx',
+            yolo_people_model_path='path_to_yolo_people_model.pt'
+        )
+
+        # Предсказание
+        error_list = checker.check_errors(uploaded_file)
+        if error_list:
+            for error in error_list:
+                st.write(f"Detected error: {error}")
+        else:
+            st.write("No errors detected.")
