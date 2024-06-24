9dc070d661c691d2db2f0bef0c4fab6b213e59dc
Simeon Kolchin <sskolchin@edu.hse.ru>
2024-06-24 04:55:19 +0300

    edit readme and api

--- app/api/api.py
+++ app/api/api.py
@@ -1,21 +1,60 @@
-from fastapi import FastAPI, UploadFile, File
-from ml.ml import LogoErrorChecker
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
+from app.ml.ml import LogoErrorChecker
 from PIL import Image
 import io
+import tempfile
 import os
 
 app = FastAPI()
 
-# Инициализация класса проверки ошибок
 checker = LogoErrorChecker()
 
 @app.post("/check_errors/")
 async def check_errors(file: UploadFile = File(...)):
-    image = Image.open(io.BytesIO(await file.read())).convert('RGB')
-    image.save("temp.jpg")
+    """
+    Принимает изображение в виде файла, проверяет его на наличие ошибок с использованием различных моделей и возвращает результат в формате JSON.
 
-    # Предсказание
-    error_list = checker.check_errors("temp.jpg")
-    os.remove("temp.jpg")
+    Параметры:
+    - file: UploadFile - загружаемый файл изображения, который нужно проверить.
+
+    Возвращает:
+    - JSONResponse: результат проверки изображения, включающий список ошибок и информацию о найденных логотипах.
     
-    return {"errors": error_list}
+    Пример ответа:
+    {
+        "errors": [],
+        "ocr_class": "some_class",
+        "bbox_results": [
+            {
+                "bbox": [36, 2, 313, 246],
+                "cropped_class": "some_class",
+                "errors": ["Неправильное направление"],
+                "ocr_class": "some_class",
+                "color_class": "some_color"
+            }
+        ]
+    }
+
+    Возможные ошибки:
+    - HTTPException 500: если произошла ошибка при обработке изображения или выполнении модели.
+    """
+    try:
+        image_data = await file.read()
+        image = Image.open(io.BytesIO(image_data)).convert('RGB')
+
+        with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as tmp_file:
+            image.save(tmp_file, format='PNG')
+            tmp_file_path = tmp_file.name
+
+        result = checker.check_errors(image)
+        os.remove(tmp_file_path)
+
+        return JSONResponse(content=result)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
