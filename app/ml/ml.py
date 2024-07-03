290c9396883e4457276b7e9e9feaa885e0598158
Simeon Kolchin <simeon.kolchin@icloud.com>
2024-07-03 19:13:25 +0300

    test_ml

--- app/ml/ml.py
+++ app/ml/ml.py
@@ -6,6 +6,23 @@ from app.ml.ocr import OCR
 from app.ml.search_people import SearchPeople
 from app.ml.logo_detector import LogoDetector
 from app.ml.color_checker import ColorChecker
+from PIL import Image
+
+
+# Чет лень это в utils пихать, но код будет выглядеть более красиво так что решил сделать)
+# (больше для себя, а то в ml.py много всего:) )
+def save_image_to_temp_file(image: Image.Image) -> str:
+    """
+    Сохраняет изображение во временный файл и возвращает путь к этому файлу.
+    
+    :param image: Объект PIL.Image, представляющий изображение.
+    :return: Строка с путем к временному файлу.
+    """
+    with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as tmp_file:
+        image.save(tmp_file, format='PNG')
+        tmp_file_path = tmp_file.name
+    return tmp_file_path
+
 
 class LogoErrorChecker:
     def __init__(self,
@@ -54,32 +71,29 @@ class LogoErrorChecker:
         - color_class (str): Класс логотипа, определённый по цвету.
         """
 
+        bboxes_info = []
         result = {
-            'errors': [],
-            'ocr_class': None,
-            'bbox_results': []
+            'bbox_results': [],
+            'full_ocr_class': None, # Если не None - надо вывести
+            'people': None # Если не None - надо вывести
         }
         
-        # Сохраняем изображение во временный файл
-        with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as tmp_file:
-            image.save(tmp_file, format='PNG')
-            tmp_file_path = tmp_file.name
+        tmp_file_path = save_image_to_temp_file(image)
 
-        
-        
         # Используем временный файл для предсказаний
         bboxes, norm_bboxes = self.logo_detector.predict(tmp_file_path)
 
-        if not bboxes:
-            result['errors'].append('логотипы не найдены')
-
         for bbox, norm_bboxes in zip(bboxes, norm_bboxes):
-            bbox_result = {
-                'bbox': bbox,
-                'cropped_class': None,
-                'errors': [],
-                'ocr_class': None,
-                'color_class': []
+            bbox_pred = {
+                'cropped_class': None, # Что распознала нейронка
+                'ocr_class': None, # Что распознала OCR система
+                'color_class': [] # Возможные классы
+            }
+
+            bbox_info = {
+                'bbox': bbox[:4], # координаты логотипа
+                'errors': [], # строки об ошибках
+                'info': [], # строки с инфой в целом, какой класс и тд
             }
 
             bbox_width = norm_bboxes[2] - norm_bboxes[0]
@@ -88,35 +102,43 @@ class LogoErrorChecker:
 
             print(bbox_area)
             if bbox_area < 0.01:
-                bbox_result['errors'].append('Логотип находится слишком далеко / он слишком маленький')
+                bbox_info['errors'].append('Логотип занимает меньше 1% фотографии, скорее всего он находится слишком далеко / он слишком маленький')
 
             cropped_image = image.crop((bbox[0], bbox[1], bbox[2], bbox[3]))
 
-            with tempfile.NamedTemporaryFile(suffix='.png', delete=False) as tmp_small_file:
-                cropped_image.save(tmp_small_file, format='PNG')
-                tmp_small_file_path = tmp_small_file.name
+            tmp_small_file_path = save_image_to_temp_file(cropped_image)
 
-            result['ocr_class'] = self.ocr_model.predict(tmp_small_file_path, threshold=0.34)
+            bbox_pred['ocr_class'] = self.ocr_model.predict(tmp_small_file_path, threshold=0.34)
+            bbox_pred['cropped_class'] = self.crop_classificator.predict(cropped_image)
+            bbox_pred['color_class'] = self.color_checker.run(cropped_image)
 
-            bbox_result['cropped_class'] = self.crop_classificator.predict(cropped_image)
-
+            # Находим ошибки
             good_bbox = self.check_classificator.predict(cropped_image)
             if not good_bbox:
-                bbox_result['errors'] = 'Некорректный логотип'
+                bbox_info['errors'].append('Некорректный логотип (неизвестно почему)')
 
-            top_5_color_classes = self.color_checker.run(cropped_image)
-            bbox_result['color_class'] = [str(k) + ' ' + str(v) + '%' for k, v in top_5_color_classes]
+            if self.direction_classificator.predict(cropped_image) < 0 and bbox_pred['cropped_class'] != 'безопасные качественные дороги':
+                bbox_info['errors'].append('Неправильное направление или присутствуют другие искажения логотипа (искажение луча, неправильная форма логотипа и прочее)')
 
-            if self.direction_classificator.predict(cropped_image) < 0 and bbox_result['cropped_class'] != 'dorogi':
-                bbox_result['errors'].append('Неправильное направление или присутствуют другие искажения логотипа (искажение луча, неправильная форма логотипа и прочее)')
+            # Добавить проверку на шум, но это нужно тюнить модельку, возможно сделаю
 
-            #TODO: добавить проверку цвета
+            
+            if bbox_pred['ocr_class'] is not None:
+                ocr_class = bbox_pred['ocr_class']
+                
+                if bbox_pred['ocr_class'] in bbox_pred['color_class']:
+                    bbox_info['info'].append(f'Логотип класса: {bbox_pred["ocr_class"]}')
+            
 
-            result['bbox_results'].append(bbox_result)
+            bbox_info['info'].append('ОТПРАВЛЯЮ ЧТО ТО')
+
+            result['bbox_results'].append(bbox_info)
 
         if self.people_searcher.detect(tmp_file_path):
-            result['errors'].append('Найден человек на фото')
+            result['people'] = 'Найден человек на фото'
 
-        result['ocr_class'] = self.ocr_model.predict(tmp_file_path)
+        full_ocr_class = self.ocr_model.predict(tmp_file_path)
+        if full_ocr_class is not None:
+            result['full_ocr_class'] = full_ocr_class
 
         return result
